45ecd635d7e509f515486134d18bbc5129d610ed
Aman <abhriguvansh99@gmail.com>
2020-11-28 21:41:05 -0800

    added buttons for sql queries (3 of them)

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>CSDS 341 Final Project</h1>
+      <h2>Aman, Rishik, Lauren, Jack</h2>
+      <div id='most-wins-afc' className='button-div'>
+        <button>
+          Find the team in the AFC division who has the best record
+        </button>
+      </div>
+      <div id='team-best-kicker' className='button-div'>
+        <button>
+          Get name of team who has the kicker with most field goals made
+        </button>
+      </div>
+      <div id='chargers-rbs' className='button-div'>
+        <button>Find number of runningbacks on the Chargers</button>
+      </div>
     </div>
   );
 }
