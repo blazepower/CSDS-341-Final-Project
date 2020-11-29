75886b2876dfbba502bdf3c535aecf23187108f1
Rishik Hombal <hombalrishik@gmail.com>
2020-11-28 22:33:32 -0500

    added more sample queries

--- backend/src/main/java/com/csds341/football/tables/TeamRepository.java
+++ backend/src/main/java/com/csds341/football/tables/TeamRepository.java
@@ -12,4 +12,28 @@ public interface TeamRepository extends JpaRepository<Team, Integer> {
                     "group by w.team.teamId"
     )
     Integer getId();
+
+    @Query(
+            "select t.teamId from Team t where t.division = 'NFC'"
+    )
+    Integer getTeamIdOfTeamNFC();
+
+    @Query(
+            "select t.teamName from Team t, Kicker k " +
+                    "where k.team.teamId = t.teamId " +
+                    "and k.fgMade = " +
+                    "(select max(k1.fgMade) from Kicker k1 " +
+                    "where k1.team.teamId = k.team.teamId)"
+    )
+    String teamWithKickerWithMostFGMade();
+
+    @Query(
+            "select t.teamName from Team t " +
+                    "where t.division = 'AFC' " +
+                    "and t.record = " +
+                    "(select max(t1.record) from Team t1 " +
+                    "where t.teamId = t1.teamId)"
+    )
+    String teamNameOfAFCWinner();
+
 }
